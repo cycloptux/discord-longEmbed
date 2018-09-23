e4fc473e193b50021d68fc35e09ffb53fe30b021
Alessandro Impellizzeri <cycloptux@users.noreply.github.com>
2018-09-23 14:41:47 +0200

    Create longEmbed.js

---
+++ longEmbed.js
@@ -0,0 +1,48 @@
+const longEmbed = function (Bot, longTextOrArray, textSeparator, embedTitle, embedColor, pre_or_post) {
+    // PRE: textSeparator is appended to the current message, before the split
+    // POST: textSeparator is prepended to the following message, after the split
+    pre_or_post = typeof pre_or_post !== "undefined" ? pre_or_post : "pre";
+    var embed = new Discord.RichEmbed();
+    if (Bot !== null) embed.setFooter(Bot.user.username, Bot.user.avatarURL).setTimestamp();
+    if (embedTitle !== null) embed.setTitle(embedTitle);
+    if (embedColor !== null) embed.setColor(embedColor);
+
+    if (Array.isArray(longTextOrArray)) {
+        var arrayOfText = longTextOrArray;
+        var longText = arrayOfText.join(textSeparator);
+    } else {
+        var reObj = new RegExp(regExpEscape(textSeparator), "g");
+        arrayOfText = longTextOrArray.split(reObj);
+        longText = longTextOrArray;
+    }
+
+    if (longText.length <= 2048) return embed.setDescription(longText);
+    else {
+        var tmsg = "", msglength = 0, msgcount = 0, m = "", cumulativeMsgLength = 0;
+        for (var i = 0; i < arrayOfText.length; i++) {
+            if (pre_or_post === "pre") m = arrayOfText[i] + textSeparator;
+            else if (pre_or_post === "post") {
+                if (i === 0) m = arrayOfText[i];
+                else m = textSeparator + arrayOfText[i];
+            }
+            msglength += m.length;
+            if (msgcount > 24 || cumulativeMsgLength + msglength > 6000) {
+                // OVER QUOTA
+                embed.setFooter("Max number of characters/fields entries reached. There may be more entries, but these were truncated.");
+                break;
+            }
+            if ((msgcount === 0 && msglength < 2048) || (msgcount > 0 && msglength < 1024)) {
+                tmsg = tmsg.concat(m);
+            } else {
+                if (msgcount === 0) embed.setDescription(tmsg);
+                else embed.addField("More...", tmsg);
+                cumulativeMsgLength += msglength;
+                tmsg = m;
+                msglength = m.length;
+                msgcount++;
+            }
+        }
+        if (!(msgcount > 24 || cumulativeMsgLength + msglength > 6000)) embed.addField("More...", pre_or_post === "pre" ? tmsg.slice(0, -textSeparator.length) : tmsg);
+        return embed;
+    }
+};
